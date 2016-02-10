81594338c771a28688604da0c4c076a62b76bc1d
Kazuki <kkaito@ncsu.edu>
2016-02-09 23:37:17 -0500

    Added some elements
    I added a text field and a select box. Now I can select CPU level and
    choose to play against AI. I will finish implementing soon. Too bad, I
    have a test tomorrow :(

--- manager.js
+++ manager.js
@@ -13,7 +13,17 @@ function init(){
 
 function clicked(event){
 	rect = event.target.getBoundingClientRect();
-	x = event.pageX-rect.left,
+	x = event.pageX-rect.left;
 	y = event.pageY-rect.top;
 	b.setStone(x,y);
+}
+
+function disclick(event){
+	rect = event.target.getBoundingClientRect();
+	x = event.pageX-rect.left;
+	y = event.pageY-rect.top;
+	if(x>(display.offsetWidth/2))
+		alert("RightSide clicked: "+x+","+display.clientWidth/2);
+	else
+		alert("LeftSide Clicked: "+x+","+display.clientWidth/2);
 }
